b80c6edd309d36ca1c4f331b25ce23142419c59a
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-12-09 19:39:51 -0600

    feat: distro detection

--- src/packages/utils.sh
+++ src/packages/utils.sh
@@ -10,8 +10,27 @@ declare -x OPERATING_SYSTEM=""
 # of most major distributions, as well as anything running systemd
 declare -x LINUX_DISTRO_OS_IDENTIFICATION_FILE="/etc/os-release"
 
+LINUX_DISTRO=""
+readonly UBUNTU_DISTRO="ubuntu"
+readonly DEBIAN_DISTRO="debian"
+readonly FEDORA_DISTRO="fedora"
+readonly RHEL_DISTRO="rhel"
+readonly CENTOS_DISTRO="centos"
+
+declare -x LINUX_DISTRO_FAMILY=""
+readonly DEBIAN_DISTRO_FAMILY=${DEBIAN_DISTRO}
+readonly FEDORA_DISTRO_FAMILY=${FEDORA_DISTRO}
+
 function error() {
   echo "[swellaby_dotfiles]: $*" >&2
+}
+
+function set_debian_variables() {
+  LINUX_DISTRO_FAMILY=${DEBIAN_DISTRO_FAMILY}
+}
+
+function set_fedora_variables() {
+  LINUX_DISTRO_FAMILY=${FEDORA_DISTRO_FAMILY}
 }
 
 function set_linux_variables() {
@@ -21,7 +40,19 @@ function set_linux_variables() {
     exit 1
   fi
 
-  return 0
+  local id
+  id=$(grep -oP '(?<=^ID=).+' ${LINUX_DISTRO_OS_IDENTIFICATION_FILE} | tr -d '"')
+  LINUX_DISTRO=$id
+  echo "Detected Linux distro: ${LINUX_DISTRO}"
+
+  case $id in
+    ${DEBIAN_DISTRO}) set_debian_variables ;;
+    ${UBUNTU_DISTRO}) set_debian_variables ;;
+    ${FEDORA_DISTRO}) set_fedora_variables ;;
+    ${RHEL_DISTRO}) set_fedora_variables ;;
+    ${CENTOS_DISTRO}) set_fedora_variables ;;
+    *) error "Unsupported distro ${LINUX_DISTRO}" && exit 1 ;;
+  esac
 }
 
 function set_macos_variables() {
@@ -42,6 +73,8 @@ function initialize() {
   readonly unix_name
   readonly OPERATING_SYSTEM
   readonly LINUX_DISTRO_OS_IDENTIFICATION_FILE
+  readonly LINUX_DISTRO
+  readonly LINUX_DISTRO_FAMILY
 }
 
 # Don't auto initialize during when sourced for running tests
