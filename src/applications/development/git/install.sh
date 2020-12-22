6fe0fa588e62d26e70633a98f395c5d28bd73b14
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-12-22 14:06:30 -0600

    refactor: tweak git installers to account for dir changes

--- src/applications/development/git/install.sh
+++ src/applications/development/git/install.sh
@@ -2,7 +2,7 @@
 
 readonly CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
-source "${CURRENT_DIR}/../../utils.sh"
+source "${CURRENT_DIR}/../../../utils.sh"
 source "${CURRENT_DIR}/git.sh"
 
 install_git
