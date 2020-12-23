fb4938b078a49704b9f1820949f58c817f01051a
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-12-23 12:26:58 -0600

    tests: add unit tests for slack installer

---
+++ tests/unit/applications/chat/common.sh
@@ -0,0 +1,10 @@
+# shellcheck shell=bash
+
+# shellcheck source=tests/test_helpers.sh
+source "${BATS_TEST_DIRNAME}/../../../test_helpers.sh"
+# Use is verified, known to be consumed
+# shellcheck disable=SC2034
+readonly CHAT_DIRECTORY="${BATS_TEST_DIRNAME}/../../../../${APPLICATIONS_CHAT_DIRECTORY_PATH_FROM_ROOT}"
+
+load "../../../../${BATS_SUPPORT_LOAD_PATH_FROM_ROOT}"
+load "../../../../${BATS_ASSERT_LOAD_PATH_FROM_ROOT}"
