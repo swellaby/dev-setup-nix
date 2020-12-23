af414a36aad2345a7f5e41fabf55f0660d20904e
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-12-23 17:48:16 -0600

    fix: slack snap prefix arg

--- src/applications/chat/slack/slack.sh
+++ src/applications/chat/slack/slack.sh
@@ -4,6 +4,7 @@ function install_slack() {
   install \
     --application-name "Slack" \
     --snap-name "slack" \
+    --snap-prefix "--classic" \
     --prefer-snap \
     --mac-package-name "slack" \
     --mac-package-prefix "--cask"
