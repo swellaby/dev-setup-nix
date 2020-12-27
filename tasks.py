c0f1aaf1f8ad5cbbf1b0f5672655870398130714
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-12-26 21:35:22 -0600

    chore: add invoke tasks

---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import task
+
+
+def black(c, check):
+    cmd = f"black . --line-length=79 {'--check' if check is True else ''}"
+    return c.run(cmd)
+
+
+@task(aliases=["f"])
+def format(c):
+    return black(c, False)
+
+
+@task(aliases=["cf", "fc"])
+def check_format(c):
+    return black(c, True)
+
+
+@task(aliases=["l", "lp"])
+def lint(c):
+    return c.run("pycodestyle .")
