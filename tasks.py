0af9fccdca442a5ab75e063d1a2900323080c96d
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-12-26 22:02:02 -0600

    chore: add shell linting to invoke tasks

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ from invoke import task
 
 def black(c, check):
     cmd = f"black . --line-length=79 {'--check' if check is True else ''}"
-    return c.run(cmd)
+    return c.run(cmd, pty=True)
 
 
 @task(aliases=["f"])
@@ -16,6 +16,20 @@ def check_format(c):
     return black(c, True)
 
 
-@task(aliases=["l", "lp"])
+@task(aliases=["lp"])
+def lint_python(c):
+    return c.run("pycodestyle .", pty=True)
+
+
+@task(aliases=["ls"])
+def lint_shell(c):
+    cmd = (
+        "find . -type f \( -name '*.sh' -o -name '*.bats' \) "
+        "! -path '*/submodules/*' | xargs shellcheck -x"
+    )
+    return c.run(cmd, pty=True)
+
+
+@task(aliases=["l"], pre=[lint_shell, lint_python])
 def lint(c):
-    return c.run("pycodestyle .")
+    pass
