2ce299c582b489a0bb75567e6cb6fe0419994822
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-12-26 20:09:11 -0600

    refactor: minor tweak on chromium install

--- src/applications/browsers/chromium/chromium.sh
+++ src/applications/browsers/chromium/chromium.sh
@@ -1,10 +1,11 @@
 # shellcheck shell=bash
 
 function install_chromium() {
+  local package_name="chromium"
   install \
     --application-name "Chromium" \
-    --debian-family-package-name "chromium-browser" \
-    --fedora-family-package-name "chromium" \
-    --mac-package-name "chromium" \
+    --debian-family-package-name "${package_name}-browser" \
+    --fedora-family-package-name "${package_name}" \
+    --mac-package-name "${package_name}" \
     --mac-package-prefix "--cask"
 }
