99bd12b7eb52a387bc3c7fd3a1a0d9953c1a4c5f
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-12-27 19:23:16 -0600

    chore: enable formatting of shell scripts

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
-from invoke import task
+from invoke import task, Result
+from invoke.exceptions import UnexpectedExit
 
 
 def black(c, check):
@@ -6,14 +7,98 @@ def black(c, check):
     return c.run(cmd, pty=True)
 
 
-@task(aliases=["f"])
-def format(c):
+@task(aliases=["fp"])
+def format_python(c):
     return black(c, False)
+
+
+@task(aliases=["cfp", "fcp"])
+def check_format_python(c):
+    return black(c, True)
+
+
+def shfmt_bats(c, check):
+    mode = f"{'-d' if check is True else '-w'}"
+    cmd = (
+        "result=0 && "
+        "for file in $(find . -type f \\( -name '*.bats' \\) "
+        "! -path '*/submodules/*'); do "
+        f"shfmt {mode} -i 2 -ci --ln bats $file; "
+        "if [ $? -ne 0 ]; then result=1; fi; done && "
+        "exit $result"
+    )
+    return c.run(cmd, pty=True)
+
+
+def shfmt_sh(c, check):
+    cmd = f"shfmt {'-d' if check is True else '-w'} ."
+    return c.run(cmd, pty=True)
+
+
+@task(aliases=["fsb"])
+def format_shell_bats(c):
+    return shfmt_bats(c, False)
+
+
+@task(aliases=["cfsb", "fcsb"])
+def check_format_shell_bats(c):
+    return shfmt_bats(c, True)
+
+
+@task(aliases=["fss"])
+def format_shell_sh(c):
+    return shfmt_sh(c, False)
+
+
+@task(aliases=["cfss", "fcss"])
+def check_format_shell_sh(c):
+    return shfmt_sh(c, True)
+
+
+@task(aliases=["fs"], pre=[format_shell_sh, format_shell_bats])
+def format_shell(c):
+    pass
+
+
+@task(aliases=["cfs", "fcs"])
+def check_format_shell(c):
+    sh_succeeded = True
+    try:
+        check_format_shell_sh(c)
+    except UnexpectedExit as err:
+        sh_succeeded = False
+
+    bats_succeeded = True
+    try:
+        check_format_shell_bats(c)
+    except UnexpectedExit as err:
+        bats_succeeded = False
+
+    if not sh_succeeded or not bats_succeeded:
+        raise UnexpectedExit(Result(command="check_format_shell", exited=1))
+
+
+@task(aliases=["f"], pre=[format_shell, format_python])
+def format(c):
+    pass
 
 
 @task(aliases=["cf", "fc"])
 def check_format(c):
-    return black(c, True)
+    shell_succeeded = True
+    try:
+        check_format_shell(c)
+    except UnexpectedExit as err:
+        shell_succeeded = False
+
+    python_succeeded = True
+    try:
+        check_format_python(c)
+    except UnexpectedExit as err:
+        python_succeeded = False
+
+    if not shell_succeeded or not python_succeeded:
+        raise UnexpectedExit(Result(command="check_format", exited=1))
 
 
 @task(aliases=["lp"])
