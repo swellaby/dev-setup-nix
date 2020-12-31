6aa1ba90cd819cd2f0aa3b4499303c6dfef58c77
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-12-31 14:00:11 -0600

    tests: add some helpers for docker unit tests

---
+++ tests/unit/applications/misc/docker/common.sh
@@ -0,0 +1,17 @@
+# shellcheck shell=bash
+
+# shellcheck source=tests/test_helpers.sh
+source "${BATS_TEST_DIRNAME}/../../../../test_helpers.sh"
+# Use is verified, known to be consumed
+# shellcheck disable=SC2034
+readonly MISC_DIRECTORY="${BATS_TEST_DIRNAME}/../../../../../${APPLICATIONS_MISC_DIRECTORY_PATH_FROM_ROOT}"
+# shellcheck source=src/applications/misc/docker/docker.sh
+source "${MISC_DIRECTORY}/docker/docker.sh"
+readonly DOCKER_DIRECTORY="${MISC_DIRECTORY}/docker"
+# Use is verified, known to be consumed
+# shellcheck disable=SC2034
+readonly UTILS_SOURCE_PATH="${BATS_TEST_DIRNAME}/../../../../../src/utils.sh"
+declare -xr DOCKER_TEST_PREFIX="${APPLICATIONS_MISC_SUITE_PREFIX}::docker"
+
+load "../../../../../${BATS_SUPPORT_LOAD_PATH_FROM_ROOT}"
+load "../../../../../${BATS_ASSERT_LOAD_PATH_FROM_ROOT}"
