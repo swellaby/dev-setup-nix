459d48f063507ba7ac6d2a214118e761dfacfb96
Caleb Cartwright <caleb.cartwright@outlook.com>
2021-01-01 10:23:20 -0600

    tests: add unit tests for dotnet installer

--- tests/unit/applications/development/common.sh
+++ tests/unit/applications/development/common.sh
@@ -8,6 +8,8 @@ readonly DEVELOPMENT_DIRECTORY="${BATS_TEST_DIRNAME}/../../../../${APPLICATIONS_
 # Use is verified, known to be consumed
 # shellcheck disable=SC2034
 readonly MISC_DIRECTORY="${BATS_TEST_DIRNAME}/../../../../${APPLICATIONS_MISC_DIRECTORY_PATH_FROM_ROOT}"
+# shellcheck disable=SC2034
+readonly UTILS_SOURCE_PATH="${BATS_TEST_DIRNAME}/../../../../src/utils.sh"
 
 load "../../../../${BATS_SUPPORT_LOAD_PATH_FROM_ROOT}"
 load "../../../../${BATS_ASSERT_LOAD_PATH_FROM_ROOT}"
