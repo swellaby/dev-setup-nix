744abea26f359d5852ca717c7f26d1b8efe7347c
Caleb Cartwright <caleb.cartwright@outlook.com>
2021-01-10 11:23:16 -0600

    refactor: rename entry function on security tools installer

--- src/applications/security/install.sh
+++ src/applications/security/install.sh
@@ -5,4 +5,4 @@ readonly APPLICATION_SECURITY_DIR_FOR_BIN=$(dirname "${BASH_SOURCE[0]}")
 source "${APPLICATION_SECURITY_DIR_FOR_BIN}/../../utils.sh"
 source "${APPLICATION_SECURITY_DIR_FOR_BIN}/security.sh"
 
-install_security_tools_bin "$@"
+install_security_tools "$@"
