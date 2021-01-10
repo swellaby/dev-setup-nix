0a101cbbf2854df472d10427b9517a482632c5f6
Caleb Cartwright <caleb.cartwright@outlook.com>
2021-01-10 11:30:23 -0600

    refactor: updates for new browser directory name

--- src/applications/browser/install.sh
+++ src/applications/browser/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-readonly APPLICATION_BROWSERS_DIR_FOR_BIN=$(dirname "${BASH_SOURCE[0]}")
+readonly APPLICATION_BROWSER_DIR_FOR_BIN=$(dirname "${BASH_SOURCE[0]}")
 
-source "${APPLICATION_BROWSERS_DIR_FOR_BIN}/../../utils.sh"
-source "${APPLICATION_BROWSERS_DIR_FOR_BIN}/browsers.sh"
+source "${APPLICATION_BROWSER_DIR_FOR_BIN}/../../utils.sh"
+source "${APPLICATION_BROWSER_DIR_FOR_BIN}/browser.sh"
 
 install_browsers_tools_bin "$@"
