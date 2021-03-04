6f46c4cfed48ee8fa5fc5600fe671ce45aec766b
Caleb Cartwright <caleb.cartwright@outlook.com>
2021-03-03 22:38:16 -0600

    test: refactor some boilerplate utils for note apps

---
+++ tests/unit/applications/note/common.sh
@@ -0,0 +1,13 @@
+# shellcheck shell=bash
+
+# shellcheck source=tests/test_helpers.sh
+source "${BATS_TEST_DIRNAME}/../../../test_helpers.sh"
+# Use is verified, known to be consumed
+# shellcheck disable=SC2034
+readonly NOTE_DIRECTORY="${BATS_TEST_DIRNAME}/../../../../${APPLICATIONS_NOTE_DIRECTORY_PATH_FROM_ROOT}"
+# Use is verified, known to be consumed
+# shellcheck disable=SC2034
+readonly UTILS_SOURCE_PATH="${BATS_TEST_DIRNAME}/../../../../src/utils.sh"
+
+load "../../../../${BATS_SUPPORT_LOAD_PATH_FROM_ROOT}"
+load "../../../../${BATS_ASSERT_LOAD_PATH_FROM_ROOT}"
